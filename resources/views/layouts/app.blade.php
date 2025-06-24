7ca842ff11fec1c60ea3d429d7e37d9115407ba0
Ludensburger <mendozaryu08@gmail.com>
2025-06-24 15:53:55 +0800

    Add navigation bar to layout for improved site navigation

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,6 +8,16 @@
 </head>
 
 <body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+            <a class="navbar-brand" href="{{ url('/') }}">Libretto</a>
+            <div class="navbar-nav">
+                <a class="nav-link" href="{{ route('books.index') }}">Books</a>
+                <a class="nav-link" href="{{ route('authors.index') }}">Authors</a>
+                <a class="nav-link" href="{{ route('genres.index') }}">Genres</a>
+            </div>
+        </div>
+    </nav>
     <div class="container py-4">
         @yield('content')
     </div>
