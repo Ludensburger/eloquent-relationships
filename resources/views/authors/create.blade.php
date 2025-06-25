1abad34d0cd955b193c0ecc3dcd2ea381bfdd6b3
Ludensburger <mendozaryu08@gmail.com>
2025-06-25 17:07:37 +0800

    Refactor spacing and formatting in author creation form

--- resources/views/authors/create.blade.php
+++ resources/views/authors/create.blade.php
@@ -11,18 +11,18 @@
                 <div class="card-body">
                     <form action="{{ route('authors.store') }}" method="POST">
                         @csrf
-                        
+
                         <div class="mb-4">
                             <label for="name" class="form-label fw-bold">
                                 <i class="fas fa-user"></i> Author Name
                             </label>
-                            <input type="text" 
-                                   class="form-control form-control-lg @error('name') is-invalid @enderror" 
-                                   id="name" 
-                                   name="name" 
-                                   value="{{ old('name') }}" 
-                                   placeholder="Enter author's full name"
-                                   required>
+                            <input type="text"
+                                class="form-control form-control-lg @error('name') is-invalid @enderror"
+                                id="name"
+                                name="name"
+                                value="{{ old('name') }}"
+                                placeholder="Enter author's full name"
+                                required>
                             @error('name')
                             <div class="invalid-feedback">{{ $message }}</div>
                             @enderror
