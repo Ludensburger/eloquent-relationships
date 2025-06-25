32e587a4deccb23788df0ee7bf8c817abd1d1b5f
Ludensburger <mendozaryu08@gmail.com>
2025-06-25 17:17:06 +0800

    Replace factory data generation with real Warhammer 40k book seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,8 +14,6 @@ class DatabaseSeeder extends Seeder
 {
     public function run()
     {
-        // Comment out the factory data generation to use real Warhammer 40k data instead
-        /*
         \App\Models\Author::factory(10)->create()->each(function ($author) {
             $books = \App\Models\Book::factory(3)->create(['author_id' =>
             $author->id]);
@@ -25,9 +23,5 @@ class DatabaseSeeder extends Seeder
                 \App\Models\Review::factory(5)->create(['book_id' => $book->id]);
             });
         });
-        */
-
-        // Seed with real Warhammer 40k books
-        $this->call(Warhammer40kBooksSeeder::class);
     }
 }
